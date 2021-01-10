25117a2bed693c8fbcc211f00c0ff8c317266b1d
David Peredo <daveperedo@gmail.com>
2021-01-09 18:36:11 -0800

    Fx opts bug in exchangeNetFlowVolume

--- src/lib/glassnode.js
+++ src/lib/glassnode.js
@@ -4,7 +4,7 @@ import querystring from 'querystring'
 const API_KEY = process.env.GLASSNODE_API_KEY
 const BASE_URL = 'https://api.glassnode.com/v1/metrics'
 
-export const exchangeNetFlowVolume = async (opts) => {
+export const exchangeNetFlowVolume = async (opts = {}) => {
 
   const interval = opts.interval || '24h'
 
